a1a2758f9c8be037136a4f468e2f99b4d127abb1
Keri Schreckengost <keris@live.com>
2017-08-05 12:00:33 -0600

    Friends list created

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+// ==============================================================
+// Data
+
+var friends = [
+	{
+		"name": "Arthur Dent",
+		"photo": "https://vignette4.wikia.nocookie.net/disney/images/8/84/Arthurdent.jpg/revision/latest?cb=20130416065941",
+		"scores":[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ],
+	},
+	{
+		"name": "Ford Perfect",
+		"photo": "https://betweenhimandher.files.wordpress.com/2011/09/mos-def-as-ford.jpg",
+		"scores":[ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 ],
+	},
+	{
+		"name": "Trillian",
+		"photo": "http://68.media.tumblr.com/318c41d11fee1d0b7efc3919458ffe46/tumblr_inline_mfep2aT1DJ1qbg6u8.jpg",
+		"scores": [ 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 ],
+	}
+];
+module.exports = friends;
+
+console.log("FriendArray: "+ friends);
